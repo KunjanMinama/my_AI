685c4aa6c7d47dbf669e2b7740b9db39f57c83be
KunjanMinama <231558936+KunjanMinama@users.noreply.github.com>
2026-06-13 22:51:36 +0530

    Learn the positional Encoding and made a class of it

--- model.py
+++ model.py
@@ -1,2 +1,23 @@
 """ Building the Transormer model from scratch"""
 import torch 
+import torch.nn
+
+class InputEmbeddings(nn.Module):
+
+    def __init__(self, d_modeol: int, vocab_size:int):
+        super(InputEmbeddings, self).__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size, d_model) 
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model: int, max_len: int = 5000):
+        super(PositionalEncoding, self).__init__()
+        self.d_model = d_model
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
